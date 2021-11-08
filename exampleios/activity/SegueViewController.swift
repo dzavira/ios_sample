9a059e0d231c3a4929e62bcab337ea053b7e13f5
David Mirza <davemirza0@gmail.com>
2021-11-08 14:51:39 +0700

    segue

---
+++ exampleios/activity/SegueViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SegueViewController.swift
+//  exampleios
+//
+//  Created by TRIAL on 08/11/21.
+//
+
+import UIKit
+
+class SegueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
