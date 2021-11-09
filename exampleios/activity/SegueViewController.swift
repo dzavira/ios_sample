290e2a2397ac0e2533370b5a377ca9db6623131e
David Mirza <davemirza0@gmail.com>
2021-11-09 08:46:20 +0700

    segue with data

--- exampleios/activity/SegueViewController.swift
+++ exampleios/activity/SegueViewController.swift
@@ -9,21 +9,14 @@ import UIKit
 
 class SegueViewController: UIViewController {
 
+    @IBOutlet weak var dataPesan: UILabel!
+    var Pesan: String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        dataPesan.text = Pesan
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- exampleios/activity/ViewController.swift
+++ exampleios/activity/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txPesan: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dataSeg = segue.destination as? SegueViewController else { return }
+        dataSeg.Pesan = txPesan.text!
+        
+    }
 
 }
 
