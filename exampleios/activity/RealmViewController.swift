eff1c0c90928e222051056d094d89dcceca0d7e2
David Mirza <davemirza0@gmail.com>
2021-11-23 09:56:35 +0700

    sample database realm

--- exampleios/activity/RealmViewController.swift
+++ exampleios/activity/RealmViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import RealmSwift
 
 class RealmViewController: UIViewController {
     let txNama: UITextField = UITextField(frame: CGRect(x: 95, y: 150, width: 250, height: 40))
+    let realm = try! Realm()
     override func viewDidLoad() {
         super.viewDidLoad()
      tampilTextfield()
+        addButtonProses()
     }
     func tampilTextfield(){
       
@@ -20,7 +23,53 @@ class RealmViewController: UIViewController {
         txNama.layer.cornerRadius = 15
         self.view.addSubview(txNama)
         
-       
+        let lbPsn = UILabel(frame: CGRect(x: 20,
+                                            y: 150,
+                                            width: 50,
+                                            height: 40))
+        lbPsn.text = "Pesan"
+        self.view.addSubview(lbPsn)
     }
+    func addButtonProses(){
+        let button = UIButton(frame: CGRect(x: 95,
+                                            y: 200,
+                                            width: 250,
+                                            height: 50))
+            button.setTitle("Simpan",
+                                for: .normal)
+            button.setTitleColor(.white,
+                                     for: .normal)
+            button.setTitleColor(.systemRed, for: .highlighted)
+            button.backgroundColor = .systemBlue
+            button.layer.cornerRadius = 15
+            button.addTarget(self, action: #selector(self.simpan), for: .touchUpInside)
+        self.view.addSubview(button)
+    }
+    
+    @objc func simpan(){
 
+        let simpanData = dtbsRealm()
+        simpanData.Nama = txNama.text!
+       
+        realm.beginWrite()
+        realm.add(simpanData)
+        try! realm.commitWrite()
+        
+        tampil()
+    }
+    func tampil(){
+        let tampilData = realm.objects(dtbsRealm.self)
+        var no = 1
+        for dtNama in tampilData{
+            let nm = dtNama.Nama
+            print("\(no) Nama : \(nm)")
+            no += 1
+        }
+        txNama.text = ""
+    }
+}
+
+class dtbsRealm: Object {
+    @objc dynamic var Nama: String=""
+ 
 }
